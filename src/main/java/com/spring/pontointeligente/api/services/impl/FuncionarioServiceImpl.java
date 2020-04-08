117730b297f61bb0465e6ac8dfa5c8ae67fdde98
Vítor Norberto <vithor.norberto@gmail.com>
2020-04-08 17:22:54 -0300

    adicionar service e teste do service de funcionario e lancamento

---
+++ src/main/java/com/spring/pontointeligente/api/services/impl/FuncionarioServiceImpl.java
@@ -0,0 +1,46 @@
+package com.spring.pontointeligente.api.services.impl;
+
+import com.spring.pontointeligente.api.entities.Funcionario;
+import com.spring.pontointeligente.api.repositories.FuncionarioRepository;
+import com.spring.pontointeligente.api.services.FuncionarioService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class FuncionarioServiceImpl implements FuncionarioService {
+
+    private static final Logger log = LoggerFactory.getLogger(EmpresaServiceImpl.class);
+
+    private FuncionarioRepository funcionarioRepository;
+
+    public FuncionarioServiceImpl(FuncionarioRepository funcionarioRepository) {
+        this.funcionarioRepository = funcionarioRepository;
+    }
+
+    @Override
+    public Funcionario persistir(Funcionario funcionario) {
+        log.info("Persistindo no banco de dados funcionario {}", funcionario);
+        return funcionarioRepository.save(funcionario);
+    }
+
+    @Override
+    public Optional<Funcionario> buscarPorCpf(String cpf) {
+        log.info("Procurando funcionario pelo CPF {}", cpf);
+        return Optional.ofNullable(funcionarioRepository.findByCpf(cpf));
+    }
+
+    @Override
+    public Optional<Funcionario> buscarPorEmail(String email) {
+        log.info("Procurando funcionario pelo email {}", email);
+        return Optional.ofNullable(funcionarioRepository.findByEmail(email));
+    }
+
+    @Override
+    public Optional<Funcionario> buscarPorId(Long id) {
+        log.info("Procurando funcionario pelo id: {}", id);
+        return funcionarioRepository.findById(id);
+    }
+}
---
+++ src/main/java/com/spring/pontointeligente/api/services/impl/LancamentoServiceImpl.java
@@ -0,0 +1,49 @@
+package com.spring.pontointeligente.api.services.impl;
+
+import com.spring.pontointeligente.api.entities.Lancamento;
+import com.spring.pontointeligente.api.repositories.FuncionarioRepository;
+import com.spring.pontointeligente.api.repositories.LancamentoRepository;
+import com.spring.pontointeligente.api.services.LancamentoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class LancamentoServiceImpl implements LancamentoService {
+
+    private static final Logger log = LoggerFactory.getLogger(EmpresaServiceImpl.class);
+
+    private LancamentoRepository lancamentoRepository;
+
+    public LancamentoServiceImpl(LancamentoRepository lancamentoRepository) {
+        this.lancamentoRepository = lancamentoRepository;
+    }
+
+    @Override
+    public Page<Lancamento> buscarPorFuncionarioId(Long funcionarioId, PageRequest pageRequest) {
+        log.info("Buscando lancamento pelo id {} e range de {}", funcionarioId, pageRequest);
+        return lancamentoRepository.findByFuncionarioId(funcionarioId, pageRequest);
+    }
+
+    @Override
+    public Optional<Lancamento> buscarPorId(Long id) {
+        log.info("Buscando lancamento por id: {}", id);
+        return lancamentoRepository.findById(id);
+    }
+
+    @Override
+    public Lancamento persistir(Lancamento lancamento) {
+        log.info("Salvando no banco de dados, lancamento: {}", lancamento);
+        return lancamentoRepository.save(lancamento);
+    }
+
+    @Override
+    public void remover(Long id) {
+        log.info("Deletando lancamento por id: {}", id);
+        lancamentoRepository.deleteById(id);
+    }
+}
