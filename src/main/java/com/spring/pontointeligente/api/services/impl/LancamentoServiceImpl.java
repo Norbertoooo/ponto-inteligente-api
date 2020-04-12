d4ef53787c613a0f8bd1f1afd348de679e2d6be4
Vítor Norberto <vithor.norberto@gmail.com>
2020-04-12 15:35:58 -0300

    Adiciona ehcache ao lancamento por id

--- src/main/java/com/spring/pontointeligente/api/services/impl/LancamentoServiceImpl.java
+++ src/main/java/com/spring/pontointeligente/api/services/impl/LancamentoServiceImpl.java
@@ -6,6 +6,8 @@ import com.spring.pontointeligente.api.repositories.LancamentoRepository;
 import com.spring.pontointeligente.api.services.LancamentoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -17,7 +19,7 @@ public class LancamentoServiceImpl implements LancamentoService {
 
     private static final Logger log = LoggerFactory.getLogger(LancamentoServiceImpl.class);
 
-    private LancamentoRepository lancamentoRepository;
+    private final LancamentoRepository lancamentoRepository;
 
     public LancamentoServiceImpl(LancamentoRepository lancamentoRepository) {
         this.lancamentoRepository = lancamentoRepository;
@@ -29,12 +31,14 @@ public class LancamentoServiceImpl implements LancamentoService {
         return lancamentoRepository.findByFuncionarioId(funcionarioId, pageRequest);
     }
 
+    @Cacheable("lancamentoPorId")
     @Override
     public Optional<Lancamento> buscarPorId(Long id) {
         log.info("Buscando lancamento por id: {}", id);
         return lancamentoRepository.findById(id);
     }
 
+    @CachePut("lancamentoPorId")
     @Override
     public Lancamento persistir(Lancamento lancamento) {
         log.info("Salvando no banco de dados, lancamento: {}", lancamento);
