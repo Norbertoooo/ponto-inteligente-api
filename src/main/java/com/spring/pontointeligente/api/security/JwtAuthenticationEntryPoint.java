9d7cf623bbb37b4a4c8159aae36f2eda5a532750
Vítor Norberto <vithor.norberto@gmail.com>
2020-04-13 01:03:38 -0300

    Corrige erro de usuario bloqueado na autenticacao

--- src/main/java/com/spring/pontointeligente/api/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/spring/pontointeligente/api/security/JwtAuthenticationEntryPoint.java
@@ -11,8 +11,11 @@ import java.io.IOException;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
     @Override
-    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
-        httpServletResponse.sendError(httpServletResponse.SC_UNAUTHORIZED, "Acesso negado. Você deve estar autenticado no sistema para acessar a URL solicitada.");
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                         AuthenticationException e) throws IOException, ServletException {
+        httpServletResponse.sendError(httpServletResponse.SC_UNAUTHORIZED,
+                "Acesso negado. Você deve estar autenticado no sistema para acessar a URL solicitada.");
     }
 }
